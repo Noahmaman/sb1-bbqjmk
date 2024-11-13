87ad818a765780f9b7e0a75afa09511667a812ea
Noahmaman <git@stackblitz.com>
2024-11-13 10:48:11 -0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Platform from './pages/Platform';
+import Solutions from './pages/Solutions';
+import Pricing from './pages/Pricing';
+
+function App() {
+  return (
+    <Router>
+      <div className="min-h-screen bg-white flex flex-col">
+        <Navbar />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/platform" element={<Platform />} />
+            <Route path="/solutions" element={<Solutions />} />
+            <Route path="/pricing" element={<Pricing />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
